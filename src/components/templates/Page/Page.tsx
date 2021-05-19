ed373057a30517a4be738b55c6830a91bebd4878
jglorange <jglorange@gmail.com>
2021-05-19 18:26:43 +0000

    some tweaks

--- src/components/templates/Page/Page.tsx
+++ src/components/templates/Page/Page.tsx
@@ -14,7 +14,7 @@ const Page = ({ data }: Props) => {
   )
 
   return (
-    <div className='page container py-3 pt-xl-4 fade-in'>
+    <div className='page container pt-lg-6 fade-in'>
       <div
         dangerouslySetInnerHTML={{
           __html: data.prismicPage.data.text.html,
